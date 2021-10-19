41372fb52834153f0c2120e9f4ad5f63146e2f10
ueshu10wowwow <2011046h@stu.kobe-u.ac.jp>
2021-10-19 15:11:21 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,26 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  let size = width / 8;
-  noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
+
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
+
+}
+function draw(){
+  strokeWeight(0)
+  let size=200/8
+  for(let i=0;i<8;i=i+1){
+    for(let j=0;j<8;j=j+1){
+     let k=i+j
+     if(k%2==1){
+       fill(196,196,196)
+       rect(size*i,size*j,size,size)
+        if(j<3){
+          fill(256,0,0)
+          ellipse(size*i+size/2,size*j+size/2,size,size)}
+        if(4<j){
+          fill(0,0,0)
+          ellipse(size*i+size/2,size*j+size/2,size,size)}
+        }
+     }
   }
 }
