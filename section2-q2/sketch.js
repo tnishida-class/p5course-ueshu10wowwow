cdc763394029040572abed2908b7bc04b4081ce0
ueshu10wowwow <2011046h@stu.kobe-u.ac.jp>
2021-10-19 15:10:37 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,54 +1,11 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-
-}
-function draw(){
-  let width=200
   let size = width / 8;
   noStroke();
-let i=0
-let j=0
-let k=i+j
-while(i<8){
-
- while(j<8){
-
-  //ここまでループ
-
-  if(k%2==1 && j<3){
-  fill(192,192,192)
-  rect(size*i,size*j,size,size);
-  fill(255,0,0)
-  ellipse(size*i+size/2,size*j+size/2,size)
-j+=1}
-
-//ここまで赤丸
-
-
-
-  else if(k%2==1 && 3<=j<=4){
-  fill(192,192,192)
-  rect(size*i,size*j,size,size);
-  j+=1
-}
-
-//ここまでグレー
-
-
-　
-  else (k%2==1 && 5<=j){
-  fill(192,192,192)
-  rect(size*i,size*j,size,size);
-  fill(0,0,0)
-  ellipse(size*i+size/2,size*j+size/2,size)
-j+=1}
-
-  //ここまで黒丸
-i+=1
-
-}
-}
+  for(let i = 0; i < 8; i++){
+    for(let j = 0; j < 8; j++){
+      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    }
+  }
 }
