be1f167a5b1aab8395976208044d55dca3b4d559
ueshu10wowwow <2011046h@stu.kobe-u.ac.jp>
2021-10-29 01:13:14 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,39 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
+let dsize;
+let v;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  size=200;
+  v=3;
+
 }
 
 function draw(){
+
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  count = (count + v) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  stroke(1)
+  fill(241,91,91)
+  ellipse(width / 2, height / 2,count);
+  if(keyIsPressed){v=20}
+  else{v=3}
+
 }
+
+function keyPressed(){
+ if(keyCode==LEFT_ARROW){width-=10}
+ else if(keyCode==RIGHT_ARROW){width+=10}
+ else if(keyCode==DOWN_ARROW){height+=10}
+ else if(keyCode==UP_ARROW){height-=10}
+// jump作ろうとしたけど解説見てもわからななかったです。
+    }
+
+
+ 
