9760773396124b236c23168dfbbbe122d9432f1f
ueshu10wowwow <2011046h@stu.kobe-u.ac.jp>
2021-11-10 19:17:23 +0900

    ＃５
    遅れてしまいました。すいません。

--- section5-1/sketch.js
+++ section5-1/sketch.js
@@ -1,14 +1,31 @@
-// テキスト「関数を使う」
-// 練習問題：このプログラムを改造してEUの旗を描いてみよう
+////練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
   createCanvas(200, 200);
-  background(192);
+  background(34,58,112);
+
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
-    let x = 100 + cos(theta) * 50;
-    let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
-  }
+    let a = 100 + cos(theta) * 50;
+    let b = 100 + sin(theta) * 50;
+   //星を置く座標
+   fill(255,215,0)
+   star(a,b,12)
+}
 }
 
+
+
 // ヒント：section5-2 にある star 関数をここにコピーして、 draw 内で ellipse の代わりに使おう
+function star(cx, cy, r){
+  beginShape();    // 点つなぎを始める
+  for(let i = 0; i < 5; i++){
+    const theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    const x = cx + cos(theta) * r;
+    const y = cy + sin(theta) * r;
+    vertex(x, y);  // 次につなぐ点を１つ増やす
+  }
+  endShape(CLOSE); // 点つなぎを終わる
+}
+// function draw(){
+// star(a,b,10)
+// }どうしてこれで呼び出せない？？
--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,25 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love hinatazaka46!!",20,15,'skyblue');
 }
 
-function balloon(t){
+function balloon(t,tx,ty,c){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+
+  fill(c);
+  push()
+  stroke(c)
+  rect(tx, ty, w + p * 2, h + p * 2);
+  pop()
+  push()
+  stroke(c)
+  triangle(tx,ty+2*p+h,tx+p,ty+2*p+h,tx-p,ty+3*p+h);
+  pop()
   fill(255);
-  text(t, p, h + p);
+  text(t, p+tx, h + p+ty);
+
+
 }
